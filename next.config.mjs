a0a98e676cf205ecb73896613f629b4fc2f104d6
Goatkenziee <mackquisition@gmail.com>
2026-07-05 17:57:10 -0400

    chore: add config files and .env.example for required env vars

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "img.clerk.com" },
-      { protocol: "https", hostname: "images.unsplash.com" },
-    ],
+  experimental: {
+    serverComponentsExternalPackages: ["stripe"],
   },
 };
-
 export default nextConfig;
